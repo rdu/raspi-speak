d7ee7556687b60c586650ce05da082f60af50110
Ronny Dudeck <ronny@dudeck.org>
2017-12-05 22:10:55 +0000

    ki

--- after_boot.sh
+++ after_boot.sh
@@ -1,3 +1,12 @@
 sleep 60
-usbreset /dev/bus/usb/001/003
+
+FILE1=/dev/bus/usb/001/003
+FILE2=/dev/bus/usb/001/002
+if [ -r "$FILE1" ]; then
+	usbreset $FILE1
+fi
+if [ -r "$FILE2" ]; then
+	usbreset $FILE2
+fi
+sleep 10
 docker restart listener
